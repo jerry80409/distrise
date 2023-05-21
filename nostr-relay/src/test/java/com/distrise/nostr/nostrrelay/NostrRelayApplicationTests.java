1fb02e0ed7e2bbbc001098194792cfc855931ed2
jerry80409 <jerry80409@gmail.com>
2023-05-21 13:16:58 +0800

    feat. nostr-relay modules init

---
+++ nostr-relay/src/test/java/com/distrise/nostr/nostrrelay/NostrRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distrise.nostr.nostrrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NostrRelayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
